1394aaf5ad3783709c00248402d77bc3c8f3d861
abdur967 <aburrahmanrashed967@gmail.com>
2025-06-02 02:13:04 +0600

    refactor - removed some unused code

--- src/utils/pdfGenerator.ts
+++ src/utils/pdfGenerator.ts
@@ -1,101 +1,7 @@
 "use client";
 import IBoardInfo from '@/features/boardInfo/boardInfo.interface'
 import IPreExamineeRegistration from '@/features/preExamineeRegistration/interfaces'
-// import jsreport from '@jsreport/browser-client'
 
-// jsreport.serverUrl = 'http://localhost:5488'
-
-
-
-// Helper function to convert English numbers to Bengali
-// const toBengaliNumber = (num: number | string) => {
-//   const bengaliNumbers = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯']
-//   return num.toString().replace(/[0-9]/g, (d) => bengaliNumbers[parseInt(d)])
-// }
-
-// Helper function to convert number to Bengali words
-// const numberToBengaliWords = (number: number) => {
-//   const units = [
-//     '',
-//     'এক',
-//     'দুই',
-//     'তিন',
-//     'চার',
-//     'পাঁচ',
-//     'ছয়',
-//     'সাত',
-//     'আট',
-//     'নয়'
-//   ]
-//   const teens = [
-//     'দশ',
-//     'এগার',
-//     'বার',
-//     'তের',
-//     'চৌদ্দ',
-//     'পনের',
-//     'ষোল',
-//     'সতের',
-//     'আঠার',
-//     'ঊনিশ'
-//   ]
-//   const tens = [
-//     '',
-//     'দশ',
-//     'বিশ',
-//     'ত্রিশ',
-//     'চল্লিশ',
-//     'পঞ্চাশ',
-//     'ষাট',
-//     'সত্তর',
-//     'আশি',
-//     'নব্বই'
-//   ]
-//   const scales = ['', 'হাজার', 'লক্ষ', 'কোটি']
-
-//   if (number === 0) return 'শূন্য'
-
-//   const processGroup = (n: number, scaleIndex: number): string => {
-//     if (n === 0) return ''
-
-//     let words = ''
-
-//     if (n > 99) {
-//       words += units[Math.floor(n / 100)] + 'শত '
-//       n %= 100
-//     }
-
-//     if (n > 19) {
-//       words += tens[Math.floor(n / 10)] + ' '
-//       if (n % 10 > 0) words += units[n % 10] + ' '
-//     } else if (n > 9) {
-//       words += teens[n - 10] + ' '
-//     } else if (n > 0) {
-//       words += units[n] + ' '
-//     }
-
-//     if (scaleIndex > 0 && words !== '') {
-//       words += scales[scaleIndex] + ' '
-//     }
-
-//     return words
-//   }
-
-//   let result = ''
-//   let remaining = number
-//   let scaleIndex = 0
-
-//   while (remaining > 0) {
-//     const group = remaining % 1000
-//     if (group > 0) {
-//       result = processGroup(group, scaleIndex) + result
-//     }
-//     remaining = Math.floor(remaining / 1000)
-//     scaleIndex++
-//   }
-
-//   return result.trim()
-// }
 
 interface ReceiptData {
   registrationData: Partial<IPreExamineeRegistration>
@@ -141,7 +47,7 @@ export const generatePreExamineeReceipt = async (data: ReceiptData) => {
 
    
 
-      const response = await fetch('http://localhost:5490/generate-report', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_JSREPORT_SERVER_URL}/generate-pre-examinee-receipt`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
